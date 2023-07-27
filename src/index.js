b458f2f0abaac00fa4ec091de28621b080e52802
Justin_Py <58087912+justin-py@users.noreply.github.com>
2023-07-27 23:10:14 +0200

    Update Kallender ufbau

--- src/index.js
+++ src/index.js
@@ -4,35 +4,16 @@ import iCalendarPlugin from "@fullcalendar/icalendar";
 
 const calendarEl = document.querySelector("calendar");
 
-var calendar = new FullCalendar.Calendar(calendarEl, {
-
-plugins: [
-    iCalendarPlugin,
-    dayGridPlugin
-    ],
-    initialView: 'dayGridMonth',
-    events: {
-    url: 'https://mywebsite.com/icalendar-feed.ics',
-    format: 'ics' // important!
-    },
-
-headerToolbar: {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'listDay,listWeek'
-},
-
-
-views: {
-    listDay: { buttonText: 'list day' },
-    listWeek: { buttonText: 'list week' }
-},
-
-height: 'auto',
-initialView: 'listWeek',
-navLinks: true, // can click day/week names to navigate views
-editable: true,
-dayMaxEvents: true, // allow "more" link when too many events
+var calendar = new Calendar(calendarEl, {
+  initialView: "listMonth",
+  plugins: [listPlugin, iCalendarPlugin],
+  height: "auto",
+  dayMaxEvents: true, // allow "more" link when too many events
+  events: {
+    url:
+      "https://calendar.google.com/calendar/ical/elsacandybarhh%40gmail.com/public/basic.ics",
+    format: "ics"
+  }
 });
 
 calendar.render();
